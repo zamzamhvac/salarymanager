057be4ef89d64f833b3f1ab4d5d2e5abf93812be
zamzamhvac <shopzamzamhvac@gmail.com>
2025-08-03 09:15:10 +0500

    Create get-config.js

---
+++ netlify/functions/get-config.js
@@ -0,0 +1,14 @@
+exports.handler = async function(event, context) {
+    return {
+        statusCode: 200,
+        body: JSON.stringify({
+            apiKey: process.env.API_KEY,
+            authDomain: process.env.AUTH_DOMAIN,
+            databaseURL: process.env.DATABASE_URL,
+            projectId: process.env.PROJECT_ID,
+            storageBucket: process.env.STORAGE_BUCKET,
+            messagingSenderId: process.env.MESSAGING_SENDER_ID,
+            appId: process.env.APP_ID,
+        }),
+    };
+};
