21414bb15b9bffcf6162f5bab15128d288f56fc1
zamzamhvac <shopzamzamhvac@gmail.com>
2025-08-05 16:26:33 +0500

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,7 @@
-// service-worker.js ka poora code
-const CACHE_NAME = 'digital-munshi-cache-v1';
+// Naya aur Theek kiya hua code
+// Version 2: Hum ne version badal diya hai taake browser naya code install kare
+const CACHE_NAME = 'digital-munshi-cache-v2';
+
 const urlsToCache = [
     '/',
     '/index.html',
@@ -7,18 +9,27 @@ const urlsToCache = [
     'https://fonts.googleapis.com/icon?family=Material+Icons',
     'https://cdnjs.cloudflare.com/ajax/libs/firebase/9.22.2/firebase-app-compat.min.js',
     'https://cdnjs.cloudflare.com/ajax/libs/firebase/9.22.2/firebase-auth-compat.min.js',
-    'https://cdnjs.cloudflare.com/ajax/libs/firebase/9.22.2/firebase-database-compat.min.js'
+    'https://cdnjs.cloudflare.com/ajax/libs/firebase/9.22.2/firebase-database-compat.min.js',
+    'https://unpkg.com/dexie@3.2.2/dist/dexie.min.js'
 ];
 
+// Event 1: Jab Service Worker install ho (Ismein koi tabdeeli nahi)
 self.addEventListener('install', event => {
-    event.waitUntil(
-        caches.open(CACHE_NAME).then(cache => {
-            return cache.addAll(urlsToCache);
-        })
-    );
+    event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache)));
 });
 
+// Event 2: Jab app koi file maange (IS HISSE KO HUMNE BEHTAR BANAYA HAI)
 self.addEventListener('fetch', event => {
+    const requestUrl = new URL(event.request.url);
+
+    // YEH NAYI HIDAYAT HAI:
+    // Agar phone call Netlify ke function ke liye hai, to usay mat roko, seedha internet par jaane do.
+    if (requestUrl.pathname.startsWith('/.netlify/functions/')) {
+        event.respondWith(fetch(event.request));
+        return; // Yahin par ruk jao
+    }
+
+    // Baaqi tamam calls ke liye, purana tareeka istemal karo (pehly cache mein dekho).
     event.respondWith(
         caches.match(event.request).then(response => {
             return response || fetch(event.request);
