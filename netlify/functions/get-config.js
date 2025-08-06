e0822199c8adf83d9f76caa59d793152efa1b8ea
zamzamhvac <shopzamzamhvac@gmail.com>
2025-08-06 10:33:21 +0500

    Update get-config.js

--- netlify/functions/get-config.js
+++ netlify/functions/get-config.js
@@ -9,6 +9,7 @@ exports.handler = async function(event, context) {
             storageBucket: process.env.STORAGE_BUCKET,
             messagingSenderId: process.env.MESSAGING_SENDER_ID,
             appId: process.env.APP_ID,
+            reCAPTCHAKey: process.env.RECAPTCHA_SITE_KEY
         }),
     };
 };
